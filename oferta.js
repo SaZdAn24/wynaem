3dc334daa25fb9ec09f1f4ad241a4f29b3844f5b
даник <danik@MacBook-Pro-danik.local>
2025-03-04 22:02:32 +0100

    creat new block oferta

---
+++ oferta.js
@@ -0,0 +1,11 @@
+
+function openOfertaModal(title, description, imageSrc) {
+    document.getElementById("oferta-modal-title").innerText = title;
+    document.getElementById("oferta-modal-description").innerText = description;
+    document.getElementById("oferta-modal-image").src = imageSrc;  
+    document.getElementById("oferta-modal").style.display = "flex";
+}
+
+function closeOfertaModal() {
+    document.getElementById("oferta-modal").style.display = "none"; // Закрываем модальное окно
+}
