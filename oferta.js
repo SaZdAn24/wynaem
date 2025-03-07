175ff1773f3ab2f0f1bb45606f062184c4d5bfba
даник <danik@mbp-danik.home>
2025-03-07 18:47:55 +0100

    creat new function

--- oferta.js
+++ oferta.js
@@ -1,11 +1,13 @@
 function openOfertaModal(title, description, imageSrc) {
-    console.log(imageSrc); // Выводим путь к изображению в консоль для проверки
     document.getElementById("oferta-modal-title").innerText = title;
     document.getElementById("oferta-modal-description").innerText = description;
     document.getElementById("oferta-modal-image").src = imageSrc;  
     document.getElementById("oferta-modal").style.display = "flex";
 }
 
-function closeOfertaModal() {
-    document.getElementById("oferta-modal").style.display = "none"; // Закрываем модальное окно
+function closeOfertaModal(event) {
+    // Если клик был по фону (за пределами окна), то закрываем модальное окно
+    if (event.target === document.getElementById("oferta-modal")) {
+        document.getElementById("oferta-modal").style.display = "none";
+    }
 }
