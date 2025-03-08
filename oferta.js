47fac490376163c97c0eb9ff8322220b2a9a5c46
даник <danik@mbp-danik.home>
2025-03-08 11:19:11 +0100

    a lot fix

--- oferta.js
+++ oferta.js
@@ -5,9 +5,14 @@ function openOfertaModal(title, description, imageSrc) {
     document.getElementById("oferta-modal").style.display = "flex";
 }
 
-function closeOfertaModal(event) {
-    // Если клик был по фону (за пределами окна), то закрываем модальное окно
-    if (event.target === document.getElementById("oferta-modal")) {
-        document.getElementById("oferta-modal").style.display = "none";
-    }
+// Закрытие модального окна
+function closeOfertaModal() {
+    document.getElementById("oferta-modal").style.display = "none";
 }
+
+// Добавляем обработчик для закрытия по клику за пределами контента
+document.getElementById("oferta-modal").addEventListener("click", function(event) {
+    if (event.target === this) {
+        closeOfertaModal();
+    }
+});
